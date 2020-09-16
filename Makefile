a0720a6b5d2347279090aa8d8bfaa3844ab475da
Alex Eastman <alexeast@buffalo.edu>
2020-09-15 23:01:35 -0400

    Added -rdynamic flag to Makefile to allow discovery of user-defined functions that were mentioned as signal-handlers in Glade. Defined two functions in sudoku.cpp for opening and closing the single-board game window. Added buttons to single-board window for resetting, finishing, and for help. Potentially more buttons to come.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 @all:
-	g++ `pkg-config --cflags gtk+-3.0` src/sudoku.cpp -o sudoku `pkg-config --libs gtk+-3.0`
+	g++ -rdynamic `pkg-config --cflags gtk+-3.0` src/sudoku.cpp -o sudoku `pkg-config --libs gtk+-3.0`
--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,5 +1,5 @@
 /*
-* Date: 09/14/20
+* Date: 09/15/20
 * Author: Alex Eastman
 * Summary: Main program file for Sudoku
 */
@@ -7,27 +7,57 @@
 #include <gtk/gtk.h>
 #include <iostream>
 
+extern "C" {
+    void open_game_single (GtkButton *button, gpointer user_data);
+    void close_game_single (GtkButton *button, gpointer user_data);
+}
+
+GtkBuilder *builder;
+
+
+/*
+*  User-defined functions
+*/
+
+// Opens the game window for single-board games
+// TODO: Generate game board before displaying (i.e. initialize game state)
+void open_game_single (GtkButton *button, gpointer user_data) {
+  GtkWidget *window = GTK_WIDGET ( gtk_builder_get_object (builder, "game_window_single"));
+  gtk_widget_show (window);
+}
+
+// Closes the game window for single-board games
+// TODO: Clear game board after closing (i.e. reset game state)
+void close_game_single (GtkButton *button, gpointer user_data) {
+  GtkWidget *window = GTK_WIDGET ( gtk_builder_get_object (builder, "game_window_single"));
+  gtk_widget_hide (window);
+}
+
+
+
+
 
 int main (int argc, char **argv) {
+  std::cout << "Spinning up\n";
   // Every GTK application needs a window. Since we're using Glade, we'll get the
   // window from a builder.
   GtkWidget  *window;
-  GtkBuilder *builder;
 
   // Initializes everything needed to operate toolkit. Call before other functions.
   gtk_init (&argc, &argv);
 
   // Get the builder from Glade-generate UI definition. Get window from builder.
   builder = gtk_builder_new_from_file ("res/GUI.glade");
-  window  = GTK_WIDGET (gtk_builder_get_object(builder, "application_window"));
+  window  = GTK_WIDGET ( gtk_builder_get_object (builder, "application_window"));
 
   // De-reference builder after it's been used. References to builder should go
   // before this line.
-  g_object_unref (G_OBJECT (builder));
+  // g_object_unref ( G_OBJECT(builder));
 
   // Show the window. Stop application when window is closed. Start main loop.
-  gtk_widget_show (window);
+  gtk_widget_show_all (window);
   g_signal_connect (window, "destroy", gtk_main_quit, NULL);
+  gtk_builder_connect_signals (builder, NULL);
   gtk_main ();
 
   return 0;
